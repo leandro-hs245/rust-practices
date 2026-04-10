2299858394cf8b226ac0d9e3298dcb752da1a1ba
Leandro Hurtado <leandro.hs245@gmail.com>
2026-04-09 21:53:23 -0500

    Added guessing features: - Add rand::Rng to create random ranges - Add std::cmp::Ordering to match guessed number with the secret number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,11 +1,29 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
+    /* Guess the number game */
     println!("Guess the number!");
-    println!("Please input your guess: ");
-    let mut guess = String::new(); // mut stand for mutable variable, otherwise it would be immutable by default
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Couldn't read the guess");
-    println!("You guessed {guess}");
+    let secret_number = rand::rng().random_range(1..=100);
+    loop {
+        println!("Please input your guess: ");
+        let mut guess: String = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Couldn't read the guess");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(number) => number,
+            Err(_) => continue,
+        };
+        println!("You guessed {guess}");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("To small."),
+            Ordering::Greater => println!("To big."),
+            Ordering::Equal => {
+                println!("You win.");
+                break;
+            }
+        };
+    }
 }
