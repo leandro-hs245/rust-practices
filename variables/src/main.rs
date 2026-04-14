8f5a32d562a1755a75d9096a3c6f6813ad884018
Leandro Hurtado <leandro.hs245@gmail.com>
2026-04-13 21:01:36 -0500

    Add shadowing function

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,8 +1,17 @@
+// Declared in global scope, so it is a constant and cannot be changed
+const SERVER_NAME: &str = "localhost";
 fn main() {
     let mut number = 5;
     println!("The number is: {number}");
     number = 10;
     println!("The number is: {number}");
-    const SERVER_NAME: &str = "localhost";
     println!("The server name is: {SERVER_NAME}");
+    shadowing_example();
+}
+
+fn shadowing_example() {
+    let number = 1;
+    println!("The number is {number}");
+    let number = number + 1;
+    println!("The number is {number}");
 }
