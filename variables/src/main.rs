5f4d26be9177e96696f5c35a830ee1cc798454d8
Leandro Hurtado <leandro.hs245@gmail.com>
2026-04-13 20:54:29 -0500

    Use mutable and constants

---
+++ variables/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let mut number = 5;
+    println!("The number is: {number}");
+    number = 10;
+    println!("The number is: {number}");
+    const SERVER_NAME: &str = "localhost";
+    println!("The server name is: {SERVER_NAME}");
+}
